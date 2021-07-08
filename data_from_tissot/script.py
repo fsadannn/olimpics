24ab57820588cff2fdc64b8a9d339e0e7908d357
JavierOramas <javiale2000@gmail.com>
2021-07-08 00:03:08 -0400

    -

--- data_from_tissot/script.py
+++
@@ -1,9 +0,0 @@
-import os
-
-for cp,dir,files in os.walk('.'):
-    for file in files:
-        if file.endswith('.pdf'):
-            src = "\""+os.path.join(cp,file)+"\""
-            dst = "\""+os.path.join(cp,file[:-4])
-            os.system('pdftotext '+src+' '+dst+'.txt"')
-
