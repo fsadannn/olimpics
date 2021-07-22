05ae83289879aee253520af791cbebcb7ac698d1
JavierOramas <javiale2000@gmail.com>
2021-07-22 00:55:10 -0400

    mountain and road data in csv

---
+++ Track Cicling/data_from_tissot/formated_data/2019/men mountain bike-/script.py
@@ -0,0 +1,37 @@
+# rank,num,name,time
+import pandas as pd
+import json
+import re
+
+athletes = {}
+
+def fix_quotes(string):
+    return string
+    # return re.sub("(\w+):", r'"\1":', string)
+    
+
+def add_data(df,tag, year = "2019"):
+    for idx, i in df.iterrows():
+        if not 'name' in list(df.columns):
+            # print('here')
+            continue
+        if not i["name"] in athletes:
+            athletes[i["name"]] = {}
+        # athletes[i["name"]]["Country"] = i["country"]
+        athletes[i["name"]][year] = {}
+        athletes[i["name"]][year][tag] = {}
+        
+        athletes[i["name"]][year][tag]["rank"]    = str(i["rank"])       
+        athletes[i["name"]][year][tag]["Time"]    = str(i["time"])       
+
+df = pd.read_csv("2020/final.csv", sep=",")
+add_data(df, "mountain_bike", "2020")
+
+df = pd.read_csv("2019/final.csv", sep=",")
+add_data(df, "mountain_bike", "2019")
+
+
+with open("men_mountain.json", "w") as f:
+    f.write(fix_quotes(json.dumps(athletes, indent=True, skipkeys=True)))
+    
+    
